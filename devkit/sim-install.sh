8e6d6b558faf8066b8a95de78950e1d0e3c22c96
Lessica <82flex@gmail.com>
2026-06-25 01:44:23 +0800

    fix: ci
    Signed-off-by: Lessica <82flex@gmail.com>

--- devkit/sim-install.sh
+++ devkit/sim-install.sh
@@ -4,15 +4,15 @@ if [ -z "$THEOS_DEVICE_SIMULATOR" ]; then
   exit 0
 fi
 
-cd $(dirname $0)/..
+cd $(dirname $0)/.. || exit
 
 TWEAK_NAMES="DictationInKeyboard"
 
 for TWEAK_NAME in $TWEAK_NAMES; do
-  sudo rm -f /opt/simject/$TWEAK_NAME.dylib
-  sudo cp -v $THEOS_OBJ_DIR/$TWEAK_NAME.dylib /opt/simject/$TWEAK_NAME.dylib
-  sudo codesign -f -s - /opt/simject/$TWEAK_NAME.dylib
-  sudo cp -v $PWD/$TWEAK_NAME.plist /opt/simject
+  sudo rm -f "/opt/simject/$TWEAK_NAME.dylib"
+  sudo cp -v "$THEOS_OBJ_DIR/$TWEAK_NAME.dylib" "/opt/simject/$TWEAK_NAME.dylib"
+  sudo codesign -f -s - "/opt/simject/$TWEAK_NAME.dylib"
+  sudo cp -v "$PWD/$TWEAK_NAME.plist" /opt/simject
 done
 
 resim
--- devkit/sim-launch.sh
+++ devkit/sim-launch.sh
@@ -4,10 +4,10 @@ if [ -z "$THEOS_DEVICE_SIMULATOR" ]; then
   exit 0
 fi
 
-cd $(dirname $0)/..
+cd $(dirname $0)/.. || exit
 
 DEVICE_ID="6B660A64-7801-4D7B-9161-74C6737432AC"
 XCODE_PATH=$(xcode-select -p)
 
 xcrun simctl boot $DEVICE_ID
-open $XCODE_PATH/Applications/Simulator.app
+open "$XCODE_PATH/Applications/Simulator.app"
